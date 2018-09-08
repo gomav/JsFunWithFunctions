c05741ebd65e60f87e9bcad44aa9f72d1b514792
Manny Navarro <manny.navarro@gmail.com>
2018-09-08 07:24:06 -0700

    calling object and applying value through an array of strings

--- call_apply.js
+++ call_apply.js
@@ -7,7 +7,9 @@ var speedUpgrade = {
     firstSpeed: '50 more miles per hour',
     secondSpeed: '200 miles an hour'
 };
+//calling object and passing value
 run.call(speedUpgrade, speedUpgrade.secondSpeed);
-
+//applying object and passing value *needs to be an array or error
+run.apply(speedUpgrade, ['500 miles an hour']);
 
 //run('100 miles an hour');

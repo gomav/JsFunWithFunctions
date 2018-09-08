5383479bbb5727d2073fbbf4d84c371195aad7b8
Manny Navarro <manny.navarro@gmail.com>
2018-09-08 09:05:43 -0700

    using anonymous closures to run an anonymous function

---
+++ anonymous_closures.js
@@ -0,0 +1,12 @@
+'use strict';
+//standard way of accessing/calling/using a function
+/*function run(){
+    console.log('foo');
+};
+run();
+*/
+//using anonymous closures, the function is wrapped in a parenthesis to run - a
+//self-executed function 
+(function(){
+    console.log('foo');
+})();

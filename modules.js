ee563b77b3dbd6ca082b6987afd3a908881019e8
Manny Navarro <manny.navarro@gmail.com>
2018-09-11 13:27:04 -0700

    created a module and executed by adding script tag to html body

---
+++ modules.js
@@ -0,0 +1,11 @@
+'use strict';
+var run = (function(){
+
+    var speed = '199 miles per hour';
+    return {
+        speed: function(){
+            console.log(speed);
+        }
+    };
+
+})(); //anonymous, self executing function.

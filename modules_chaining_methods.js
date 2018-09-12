a39a523e6507ea2b6eeebe22e0d0aa90f6701b20
Manny Navarro <manny.navarro@gmail.com>
2018-09-12 16:36:00 -0700

    practicing modules chaining method, passing values through html

---
+++ modules_chaining_methods.js
@@ -0,0 +1,19 @@
+'use strict';
+var run=(function(){
+    var DEFAULTS={
+        topSpeed: '250 miles an hour',
+        topHorsePower: '1000 horse power'
+    };
+    return {
+        speed: function(){
+            var allArguments = arguments[0] || '';
+            var myStatements = allArguments.topSpeed || DEFAULTS. topSpeed;
+            console.log(myStatements);
+        },
+        horsePower: function(){
+            var additionalArguments = arguments[0] || '';
+            var topHorsePowerChecking = additionalArguments.topHorsePower || DEFAULTS. topHorsePower;
+            console.log(topHorsePowerChecking);
+        }
+    };
+})();

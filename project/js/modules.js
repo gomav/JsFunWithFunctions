e3296629f05e0be04b743912aadc1ab95c13dde4
Manny Navarro <manny.navarro@gmail.com>
2018-09-13 13:53:52 -0700

    Car Gallery complete, debugged, and added some comments for clarity

--- project/js/modules.js
+++ project/js/modules.js
@@ -1,12 +1,51 @@
 'use strict';
-var imageGallery = (function(){
+var imageModule = (function(){
 
     return {
+        //image object that the code will be looping through
+        carImages: {
 
-        display: function(){
+            car1: 'http://emaginas.com',
+            car2: 'http://emaginas.com',
+            car3: 'http://emaginas.com',
+            car4: 'http://emaginas.com',
+            car5: 'http://emaginas.com',
+            car6: 'http://emaginas.com',
+            car7: 'http://emaginas.com',
+            car8: 'http://emaginas.com'
+        },
 
-            console.log("By God! This shit works!");
+        //the method below inserts the images by looping
+        insertImages : function(){
 
+            var myArguments = '.' + arguments[0];
+
+            var imagesContainer = document.querySelectorAll(myArguments);
+
+            var output = "<div>";
+
+            for(var key in this.carImages){
+                //html, bootstrap, markup. Note make sure that single and double
+                //quotes are properly accounted for; also check spelling!
+                output += '<div class="col-lg-3 col-md-4 col-xs-6 thumb">';
+
+                output += '<a class="thumbnail" href="'+this.carImages[key]+'">';
+
+                output += '<img class="img-responsive" src="images/'+key+'.jpg'+'" alt=""> ';
+
+                output += '</a>';
+
+                output += '</div>';
+                //console.log(key); or
+                //console.log(output);
+            }
+
+            for(var i = 0; i < imagesContainer.length; i++){
+
+                imagesContainer[i].innerHTML = output;
+            }
+
+            output += '</div>';
         }
     };
 
